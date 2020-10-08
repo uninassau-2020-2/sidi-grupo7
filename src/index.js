322ea1e53c4cf49ceedf9d6dc410c2f4dc604041
joaovicslins <joaovic.slins@gmail.com>
2020-10-08 18:11:50 -0300

    Index feita

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-const { uri } = require('../configs/configs')
-console.log(uri)
+const port  = 8081
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const { makeConsultViaCep, renderHome } = require('./routes')
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send(`<strong>EM DESENVOLVIMENTO</strong>`)
+})
+
+app.get('/zipcode', renderHome)
+
+app.post('/zipcode', makeConsultViaCep)
+
+app.get('*', (req, res) => {
+    res.redirect('/')
+})
+
+app.listen(port, () => {
+    console.log(`The server is running on http://localhost:${port}`)
+})
